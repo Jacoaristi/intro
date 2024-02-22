d0185a6be1c1be843f4f81a20d29b23d1c5e0f2d
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-02-22 07:43:51 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi primera App!!")
 st.header("Este es el espacio para desarrollar las aplicaciones que considere de mi agrado para interfaces multimodales")
 st.write("facilmente puedo realizar backend y frontend")
-image= Image.open("imagen1.png")
+image= Image.open("1.png")
 
 st.image(image, caption='art peter giffin')
 
