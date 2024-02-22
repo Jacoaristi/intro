ac26cacb5dd9ec8aee775c5f8feb6b1e5ba601ed
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-02-22 08:01:19 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -15,7 +15,7 @@ st.subheader("Ahora usemos 2 Comlumnas")
 col1, col2 = st.columns(2)
 
 with col1: 
-  st.subeader ("Esta es la primera columna")
+  st.subheader ("Esta es la primera columna")
   st.write("Las interfaces multimodales mejoran la experiencia de usuario")
   resp = st.checkbox('Estoy de acuerdo')
   if resp:
