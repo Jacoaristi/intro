37730dbf25442477f2f79f79f49bb9ae4adb0cae
Jacoaristi <160860995+Jacoaristi@users.noreply.github.com>
2024-02-22 07:42:18 -0500

    intro.py

--- intro.py
+++ intro.py
@@ -2,3 +2,10 @@ import streamlit as st
 from PIL import Image
 
 st.title("Mi primera App!!")
+st.header("Este es el espacio para desarrollar las aplicaciones que considere de mi agrado para interfaces multimodales")
+st.write("facilmente puedo realizar backend y frontend")
+image= Image.open("imagen1.png")
+
+st.image(image, caption='art peter giffin')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
